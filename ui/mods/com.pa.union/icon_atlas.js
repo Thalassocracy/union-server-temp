1430b043b8d2de8f8fadde08b85cf5cc19510318
Thalassocracy <42811940+Thalassocracy@users.noreply.github.com>
2019-11-25 00:03:25 -0500

    Saboteur, Monarch, Hermit + performance increase on Hermit

--- ui/mods/com.pa.union/icon_atlas.js
+++ ui/mods/com.pa.union/icon_atlas.js
@@ -1,13 +1,17 @@
 var paeiouIcons = ["fab",
 "can",
 "quincy",
+"saboteur",
 "arc",
 "luddite",
 "foxhound",
 "gale",
 "ogre",
 "monitor",
+"hermit",
+"monarch",
 "power_plant",
 "crawler_factory",
+"plug",
 ];
 model.strategicIcons(model.strategicIcons().concat(paeiouIcons));
--- ui/mods/com.pa.union/shared_build.js
+++ ui/mods/com.pa.union/shared_build.js
@@ -2,14 +2,18 @@ var newBuild = {
 "/pa/units/PAEIOU/crawler/t1/fab/fab.json": ["crawler", 0,{ row: 2, column: 0, titans: true }],
 "/pa/units/PAEIOU/crawler/t1/can/can.json": ["crawler", 0,{ row: 2, column: 1, titans: true }],
 "/pa/units/PAEIOU/crawler/t1/quincy/quincy.json": ["crawler", 0,{ row: 2, column: 2, titans: true }],
+"/pa/units/PAEIOU/crawler/t1/saboteur/saboteur.json": ["crawler", 0,{ row: 2, column: 3, titans: true }],
 "/pa/units/PAEIOU/crawler/t1/arc/arc.json": ["crawler", 0,{ row: 2, column: 4, titans: true }],
 "/pa/units/PAEIOU/crawler/t2/luddite/luddite.json": ["bot", 0,{ row: 0, column: 1, titans: true }],
 "/pa/units/PAEIOU/crawler/t2/foxhound/foxhound.json": ["bot", 0,{ row: 0, column: 2, titans: true }],
 "/pa/units/PAEIOU/crawler/t2/gale/gale.json": ["bot", 0,{ row: 0, column: 3, titans: true }],
 "/pa/units/PAEIOU/heavy_crawler/t1/ogre/ogre.json": ["heavy_crawler", 0,{ row: 2, column: 1, titans: true }],
 "/pa/units/PAEIOU/heavy_crawler/t1/monitor/monitor.json": ["heavy_crawler", 0,{ row: 2, column: 2, titans: true }],
+"/pa/units/PAEIOU/mobile_defense/t1/hermit/hermit.json": ["heavy_crawler", 0,{ row: 2, column: 3, titans: true }],
+"/pa/units/PAEIOU/mobile_defense/t2/monarch/monarch.json": ["heavy_crawler", 0,{ row: 1, column: 3, titans: true }],
 "/pa/units/PAEIOU/structure/t1/power_plant/power_plant.json": ["utility", 0,{ row: 1, column: 5, titans: true }],
 "/pa/units/PAEIOU/structure/t1/crawler_factory/crawler_factory.json": ["factory", 0,{ row: 2, column: 5, titans: true }],
+"/pa/units/PAEIOU/structure/t1/plug/plug.json": ["special", 0,{ row: 2, column: 0, titans: true }],
 
 }
 if (Build && Build.HotkeyModel && Build.HotkeyModel.SpecIdToGridMap) {
